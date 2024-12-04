6d5b7e731c356d7ee153834a21e800f9734d7a2f
rHarylala <rabenamana.h@zurcher.edu.mg>
2024-12-04 17:55:24 +0300

    second commit

--- aggregate/src/App.jsx
+++ aggregate/src/App.jsx
@@ -9,61 +9,65 @@ const App = () => {
     const fetchStudents = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/students');
-        console.log('Données reçues :', response.data);
+        console.log('Données reçues :', response.data);
         setStudents(response.data);
       } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error);
+        console.error('Erreur lors de la récupération des données :', error);
       }
     };
     fetchStudents();
   }, []);
 
   return (
-    <>
-      <Navbar fluid rounded>
-        <Navbar.Brand href="#">
-          <img src="/image.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      <>
+        <Navbar fluid rounded>
+          <Navbar.Brand href="#">
+            <img src="/image.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Aggregation
           </span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse></Navbar.Collapse>
-        <Flowbite>
-          <DarkThemeToggle />
-        </Flowbite>
-      </Navbar>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse></Navbar.Collapse>
+          <Flowbite>
+            <DarkThemeToggle />
+          </Flowbite>
+        </Navbar>
 
-      <Table>
-        <Table.Head>
-          <Table.HeadCell>ID</Table.HeadCell>
-          <Table.HeadCell>Nom</Table.HeadCell>
-          <Table.HeadCell>Prénom</Table.HeadCell>
-          <Table.HeadCell>Religion</Table.HeadCell>
-          <Table.HeadCell>
-            <span className="sr-only">Modifier</span>
-          </Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
-          {students.map((student) => (
-            <Table.Row key={student.student_id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                {student.student_id}
-              </Table.Cell>
-              <Table.Cell>{student.student_nom}</Table.Cell>
-              <Table.Cell>{student.student_prenom}</Table.Cell>
-              <Table.Cell>{student.religion}</Table.Cell>
-              <Table.Cell>
-                <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                  Edit
-                </a>
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </>
+        <Table>
+          <Table.Head>
+            <Table.HeadCell>ID</Table.HeadCell>
+            <Table.HeadCell>Nom</Table.HeadCell>
+            <Table.HeadCell>Prénom</Table.HeadCell>
+            <Table.HeadCell>Religion</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Modifier</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {students.map((student, index) => (
+                <Table.Row
+                    key={`student-${index}-${student.student_id || ''}`}
+                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {student.student_id || 'N/A'}
+                  </Table.Cell>
+                  <Table.Cell>{student.student_nom || 'N/A'}</Table.Cell>
+                  <Table.Cell>{student.student_prenom || 'N/A'}</Table.Cell>
+                  <Table.Cell>{student.religion || 'N/A'}</Table.Cell>
+                  <Table.Cell>
+                    <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                      Edit
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </>
   );
 };
 
 export default App;
+
